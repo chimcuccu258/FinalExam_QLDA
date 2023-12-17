a8a26e253cac1ed57dd23fa1acfe60715aca61c7
GunterWi <boi09456@gmail.com>
2023-12-17 20:46:09 +0700

    Upload Prototype layoyt for project

---
+++ StoreForFashion/Session/ConstainCart.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StoreForFashion.Session
+{
+    public class ConstainCart
+    {
+        public static string CART = "CART";
+    }
+}
---
+++ StoreForFashion/Session/ConstaintUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StoreForFashion.Session
+{
+    public static class ConstaintUser
+    {
+        public static string ADMIN_SESSION = "ADMIN";
+        public static string USER_SESSION = "USER";
+    }
+}
