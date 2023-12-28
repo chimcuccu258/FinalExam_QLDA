7156884f44a2be024b16c53a37e7d884b0ae826a
GunterWi <boi09456@gmail.com>
2023-12-28 23:31:01 +0700

    Upload giao vien va chuc nang cua quan ly danh muc

--- StoreForFashion/Controllers/HomeController.cs
+++ StoreForFashion/Controllers/HomeController.cs
@@ -49,5 +49,90 @@ namespace StoreForFashion.Controllers
             return PartialView(list);
         }
 
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            TaiKhoanNguoiDung session = (TaiKhoanNguoiDung)Session[StoreForFashion.Session.ConstaintUser.USER_SESSION];
+            if (session != null)
+            {
+                return RedirectToAction("PageNotFound", "Error");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginAccount loginAccount)
+        {
+            if (ModelState.IsValid)
+            {
+                TaiKhoanNguoiDung tk = db.TaiKhoanNguoiDungs.Where
+                (a => a.TenDangNhap.Equals(loginAccount.username) && a.MatKhau.Equals(loginAccount.password)).FirstOrDefault();
+                if (tk != null)
+                {
+                    if (tk.TrangThai == false)
+                    {
+                        ModelState.AddModelError("ErrorLogin", "Tài khoản của bạn đã bị vô hiệu hóa !");
+                    }
+                    else
+                    {
+                        Session.Add(ConstaintUser.USER_SESSION, tk);
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("ErrorLogin", "Tài khoản hoặc mật khẩu không đúng!");
+                }
+            }
+            return View(loginAccount);
+        }
+
+        [HttpGet]
+        public ActionResult Logout()
+        {
+            Session.Remove(ConstaintUser.USER_SESSION);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult SignUp()
+        {
+            TaiKhoanNguoiDung session = (TaiKhoanNguoiDung)Session[StoreForFashion.Session.ConstaintUser.USER_SESSION];
+            if (session != null)
+            {
+                return RedirectToAction("PageNotFound", "Error");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(TaiKhoanNguoiDung tk)
+        {
+            TaiKhoanNguoiDung check = db.TaiKhoanNguoiDungs.Where
+                (a => a.TenDangNhap.Equals(tk.TenDangNhap)).FirstOrDefault();
+            if (check != null)
+            {
+                ModelState.AddModelError("ErrorSignUp", "Tên đăng nhập đã tồn tại");
+            }
+            else
+            {
+                try
+                {
+                    tk.TrangThai = true;
+                    db.TaiKhoanNguoiDungs.Add(tk);
+                    db.SaveChanges();
+                    TaiKhoanNguoiDung session = db.TaiKhoanNguoiDungs.Where(a => a.TenDangNhap.Equals(tk.TenDangNhap)).FirstOrDefault();
+                    Session[StoreForFashion.Session.ConstaintUser.USER_SESSION] = session;
+                    return RedirectToAction("Index", "Home");
+                }
+                catch (Exception)
+                {
+                    ModelState.AddModelError("ErrorSignUp", "Đăng ký không thành công. Thử lại sau !");
+                }
+            }
+
+            return View(tk);
+        }
     }
 }
